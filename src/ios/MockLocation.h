182e0470e6b315df5c8e76cfb2b285be0ebf5b8f
stephane padovani <stephane.padovani@backelite.com>
2025-03-02 12:28:11 +0100

    [fix][ios] wrong class declaration

--- src/ios/MockLocation.h
+++ src/ios/MockLocation.h
@@ -4,4 +4,5 @@
 @interface MockLocation : CDVPlugin
 
 - (void)check:(CDVInvokedUrlCommand*)command;
+
 @end
--- src/ios/MockLocation.m
+++ src/ios/MockLocation.m
@@ -1,20 +1,25 @@
 #import <CoreLocation/CoreLocation.h>
+#import "MockLocation.h"
 
-- (BOOL)check:(CDVInvokedUrlCommand*)command {
+@implementation MockLocation
+
+- (void)check:(CDVInvokedUrlCommand*)command {
     CDVPluginResult* pluginResult = nil;
 
-    if (!@available(iOS 15.0, *)) {
+    if (@available(iOS 15.0, *)) {
       CLLocationManager *locationManager = [[CLLocationManager alloc] init];
       CLLocation *location = locationManager.location;
       
       BOOL isSimulatedBySoftware = location.sourceInformation.isSimulatedBySoftware;
       BOOL isProducedByAccessory = location.sourceInformation.isProducedByAccessory;
       BOOL isSimulated = isSimulatedBySoftware || isProducedByAccessory;
-      pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBoolean:isSimulated];
+      pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:isSimulated];
     } else {
       [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-       pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBoolean:NO];
+       pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:NO];
     }
 
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
+
+@end
